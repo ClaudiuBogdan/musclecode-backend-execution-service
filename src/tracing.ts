3c1fc19896293222d4a1376069f120d4117cb7eb
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2025-02-07 12:19:36 +0200

    feat: improve OpenTelemetry tracing configuration
    - Added W3C Trace Context Propagator
    - Configured BatchSpanProcessor for trace exporting
    - Integrated NestJS core instrumentation
    - Added custom HTTP instrumentation to ignore /healthz endpoint
    - Enhanced tracing configuration with more granular control

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,4 +1,5 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
+import { W3CTraceContextPropagator } from '@opentelemetry/core';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { Resource } from '@opentelemetry/resources';
@@ -8,10 +9,14 @@ import {
   SEMRESATTRS_DEPLOYMENT_ENVIRONMENT,
 } from '@opentelemetry/semantic-conventions';
 import { config } from './config/load-config';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 
 const traceExporter = new OTLPTraceExporter({
   url: config.TRACE_ENDPOINT,
 });
+
+const propagator = new W3CTraceContextPropagator();
 
 export const otelSDK = new NodeSDK({
   resource: new Resource({
@@ -25,8 +30,23 @@ export const otelSDK = new NodeSDK({
     'k8s.namespace.name': config.K8S_NAMESPACE,
     'k8s.pod.name': config.K8S_POD_NAME,
   }),
-  traceExporter,
-  instrumentations: [getNodeAutoInstrumentations()],
+  spanProcessors: [new BatchSpanProcessor(traceExporter)],
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-http': {
+        ignoreIncomingRequestHook: (req) => {
+          // Ensure req.url is defined
+          if (!req.url) return false;
+          // Remove any query parameters from the URL
+          const urlPath = req.url.split('?')[0];
+          // Filter out only the /healthz endpoint from being instrumented
+          return urlPath === '/healthz';
+        },
+      },
+    }),
+    new NestInstrumentation(),
+  ],
+  textMapPropagator: propagator,
 });
 
 // gracefully shut down the SDK on process exit
